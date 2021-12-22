b71bca97e3050ba123397b03b1e7b34e69a79d4b
ADITYA <75035219+DEVLOPRR@users.noreply.github.com>
2021-12-22 19:58:29 +0530

    Adding font by name
    Now user can enter the font name from fonts.google.com and font will be verified if it exists and then will be added to the a array which contains all the custom fonts and will be applied to the HTML
    
    and converted major functions to asynchronous so that while rendering webpage isn't blocked.

--- src/index/index.js
+++ src/index/index.js
@@ -6,11 +6,48 @@ import introJs from "intro.js";
 import iconMap from "../../static/iconMap";
 import introSteps from "../../static/introSteps";
 
-var stylePrefix = $('#keyStyles').val(); // stylePrefix is the value of the checked radio box. this value will be used as class name of the keys to apply a particular style to the keys
-var userText = "", keysArray = "", htmlCode = "", fileSaverSupported; // Pre Defining some variables
+/* stylePrefix is the value of the checked radio box.
+   this value will be used as class name of the keys to
+   apply a particular style to the keys */
+var stylePrefix = $('#keyStyles').val();
 
-try { let isFileSaverSupported = !!new Blob; fileSaverSupported = true; }
-catch (e) { fileSaverSupported = false; }
+// Pre Defining some global variables
+var userText = "";
+var keysArray = ""
+var htmlCode = "";
+var fsSupported = false;
+
+const customFonts = JSON.parse(localStorage.getItem("customFonts")) || [
+	"Poppins", "Custom", "Roboto", "Open+Sans",
+	"Lato", "Montserrat", "Roboto+Condensed",
+	"Source+Sans+Pro", "Oswald", "Roboto+Mono",
+	"Raleway", "Ubuntu", "Nunito", "Roboto+Slab",
+	"PT+Sans", "Merriweather", "Inter", "Rubik",
+	"Nunito+Sans", "Work+Sans", "Nanum+Gothic",
+	"Fira+Sans", "Quicksand", "Titillium+Web",
+	"Barlow", "Inconsolata", "Bebas+Neue",
+	"Josefin+Sans", "Oxygen", "Source+Code+Pro",
+	"Mulish", "Yanone+Kaffeesatz", "Cabin",
+	"DM+Sans", "Abel", "Padauk", "Varela+Round",
+	"Comfortaa", "Exo+2", "Kanit", "Overpass",
+	"Ubuntu+Mono", "Staatliches", "Acme"
+];
+
+// Asynchronously Add All the fonts in #fontStyles
+(async() => {
+	for (let i = 0; i < customFonts.length; i++) {
+		let newOption = document.createElement("option")
+		let fontCode = customFonts[i]
+		newOption.innerText = fontCode.replace(/\+/g, ' ')
+		newOption.value = fontCode
+		$('#fontStyles').append(newOption).val("Poppins")
+	}
+	
+	$("#fontStyles option")[0].innerText = "Default"
+})($, customFonts, document)
+
+try { new Blob; fsSupported = true; }
+catch (e) { fsSupported = false; }
 
 // Convert base64 data to Blob
 function dataURItoBlob(dataURI) {
@@ -23,7 +60,7 @@ function dataURItoBlob(dataURI) {
 	return blob;
 }
 
-function hideLoader(hide) { // Will be used To Hide/Unhide The Loader while generating the Image.
+async function hideLoader(hide) { // Will be used To Hide/Unhide The Loader while generating the Image.
 	if (hide == true) $("#loader").css("display", "none");
 	if (hide == false) $("#loader").css("display", "block");
 }
@@ -36,7 +73,7 @@ function appendImg(url) { // appendImg function to append generated image to the
 	$("#preview-div").css("display", "block"); // And changing the output element's parent display to block
 }
 
-function saveAsPng() { // Function to generate HTML to PNG
+async function saveAsPng() { // Function to generate HTML to PNG
 	hideLoader(false)
 	let element = $('#htmlCode'), scale = prompt("Enter the Scale: ", 2);
 
@@ -58,28 +95,43 @@ function saveAsPng() { // Function to generate HTML to PNG
 	domtoimage.toPng(element[0], {
 			width: Width, // Setting The width of the output Image
 			height: Height, // Setting the height of the output image
-			style: { transform: 'scale('+scale+')', transformOrigin: 'top left'} // Setting the scale and the transform origin
+			style: { // Setting the scale and the transform origin
+				transform: 'scale('+scale+')',
+				transformOrigin: 'top left'
+			}
 		})
 		.then(function (dataUrl) {
-			if (fileSaverSupported === true) saveAs(dataURItoBlob(dataUrl), "key2Img-"+Width+"x"+Height+".png");
-			else {
+			if (fsSupported === true) {
+				saveAs(dataURItoBlob(dataUrl), "key2Img-"+Width+"x"+Height+".png");
+			} else {
 				appendImg(dataUrl);
 				$("#output-demension").text("Height: "+Height+"px, Width: "+Width);
 			}
 			hideLoader(true)
 		}) // calling the appendImg function and passing the generated image data as parameter and then changing the output div's text with the respective height and width
-		.catch(function (error) { alert('oops, something went wrong!\n' + error); hideLoader(true) }); // If something goes wrong calling the alert function with showing the error
+		.catch(function (err) {
+			console.error(err)
+			new Toast({ message: `oops, something went wrong!,\n${err}`, type: 'danger' });
+			hideLoader(true)
+		}); // If something goes wrong calling the alert function with showing the error
 }
 
-function saveAsJpeg() { // Function To Generate HTML To JPG
+async function saveAsJpeg() { // Function To Generate HTML To JPG
 	hideLoader(false) // Unhide The Loader
 	let element = $('#htmlCode');
 	let scale = prompt("Enter the Scale: ", 2);
 	let quality = prompt("Quality (0.1 - 1): ", 0.5);
 
 	if (scale == null || quality == null) return;
-	if (isNaN(scale) === false || isNaN(quality) === false) { scale = parseInt(scale); quality = parseFloat(quality) }
-	if (scale === 0 || quality === 0) { scale = 1; parseFloat(0.5); }
+	if (isNaN(scale) === false || isNaN(quality) === false) {
+		scale = parseInt(scale);
+		quality = parseFloat(quality)
+	}
+
+	if (scale === 0 || quality === 0) {
+		scale = 1;
+	}
+
 	if (quality > 1) quality = 0.5;
 
 	if (scale > 10 && !confirm("Are You Sure? Scale More than 10 Can Make your Device Lag")) {
@@ -102,7 +154,7 @@ function saveAsJpeg() { // Function To Generate HTML To JPG
 		})
 		.then(function (dataUrl) {
 			// Check if fileSaver is Supported
-			if (fileSaverSupported === true) {
+			if (fsSupported === true) {
 				// If File Saver is Supported Save The File.
 				saveAs(dataURItoBlob(dataUrl), "key2Img-"+Width+"x"+Height+".jpeg");
 			} else {
@@ -114,7 +166,7 @@ function saveAsJpeg() { // Function To Generate HTML To JPG
 		})
 		.catch((err) => {
 			console.error(err)
-			alert('oops, something went wrong!\n' + err);
+			new Toast({ message: `oops, something went wrong!,\n${err}`, type: 'danger' });
 			hideLoader(true)
 		});
 }
@@ -123,7 +175,7 @@ const replaceIco = (str) => {
 	return str.split(' ').map((s) => iconMap[s] ?? s).join(' ');
 };
 
-function renderKeys() {
+async function renderKeys() {
 	var userText = $('#text-input').val(); // User's Input Text
 	var keysArray = userText.split("+").map(replaceIco); // Split user's input with + and replace any icon code in the array items with the corresponding HTML code given in iconMap.js
 	keysArray = $.map(keysArray, $.trim); // Trim any extra space in start or end of any item in the Array
@@ -153,7 +205,7 @@ $("#keyStyles").on("change", () => { // Run a function when someone selects a ne
 	renderKeys(); // Render the keys with new selected options
 })
 
-window.onload = e => {
+window.onload = async (e) => {
 	// Cookies.remove("firstTime");
 	if (Cookies.get("firstTime") == undefined) {
 		Cookies.set('firstTime', 'no', { expires: 3652.5 }); // 3652.5 is 10 years
@@ -166,9 +218,42 @@ window.onload = e => {
 
 	htmlPreview.css('font-family', "'Poppins', sans-serif")
 
-	fontSelector.on('change', () => {
-		fontLink.attr('href', `https://fonts.googleapis.com/css?family=${fontSelector.val()}&display=swap`)
-		htmlPreview.css('font-family', `${$("#fontStyles option:selected" ).text()}, 'Poppins', sans-serif`)
+	fontSelector.on('change', async () => {
+		if (fontSelector.val() == "Custom") {
+			let fontName = prompt("Enter a font name from 'fonts.google.com'")
+
+			if (fontName == null || fontName == "") {
+				fontSelector.val("Poppins")
+				return;
+			} else if (customFonts.includes(fontName.replace(/ /g, '+').trim())) {
+				fontSelector.val(fontName.replace(/ /g, '+').trim())
+				new Toast({ message: 'The font is already in the dropdown menu.', type: 'danger' });
+				return;
+			}
+
+			fontName.trim()
+			let fontCode = fontName.replace(/ /g, '+')
+
+			try {
+				await fetch(`https://fonts.googleapis.com/css?family=${fontCode}&display=swap`)					
+			} catch (error) {
+				fontSelector.val("Poppins");
+				new Toast({ message: "Font doesn't exist.", type: 'danger' });
+				return;
+			}
+
+			let newOption = document.createElement("option")
+			newOption.innerText = fontCode
+			newOption.value = fontCode.replace(/ /g, '+')
+			$('#fontStyles').append(newOption).val(fontCode)
+			$('#font-link').attr('href', `https://fonts.googleapis.com/css?family=${fontCode}&display=swap`)
+			$('#htmlCode').css('font-family', `'${fontName}', 'Poppins', sans-serif`)
+			customFonts.push(fontCode)
+			localStorage.setItem("customFonts", JSON.stringify(customFonts))
+		} else {
+			fontLink.attr('href', `https://fonts.googleapis.com/css?family=${fontSelector.val()}&display=swap`)
+			htmlPreview.css('font-family', `'${$("#fontStyles option:selected" ).text()}', 'Poppins', sans-serif`)
+		}
 	});
 
 	$("#web-usage").on("click", () => introJs().setOptions({ steps: introSteps }).start())
